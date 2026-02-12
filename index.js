ed7f6681b3b2136dddcd08b3d558d308999218c4
MOHAFISHER <mohamedbentmim973@gmail.com>
2026-02-12 12:56:34 +0100

    i have fixed some problems

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const http = require ('http');
 const fs= require('fs');
+const querystring= require('querystring');
 const server =http .createServer((req, res)=>{
     if(req.url=== "/style.css"){
         res.writeHead(200, {"Content-type": "text/css"});
         res.end(fs.readFileSync("style.css"));
-    } else if(req.method === 'POST'){
+    } else if(req.method === "POST"){
         let body="";
         req.on('data', chunk => {
-            body += chunk,toString();
+            body += chunk.toString();
         });
         req.on('end', ()=> {
-            try{
-                const newOrder = JSON.parse(body);
-            } catch (error){
-                res.writeHead(400, {'Content-Type' : 'application/json'});
-                res.end(JSON.stringify({error:'Invalid JSON'}));
-            }
+            const formData= querystring.parse(body);
+            console.log(formData);
+            
+            res.writeHead(200,{"Content-type":"application/json"});
+            res.end(JSON.stringify({
+                message:"Order received",
+                data:formData
+            }));
         });
     }
 
